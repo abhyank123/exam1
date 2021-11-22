79551fef81ef0b7547ab43cddb95a4ef4a2e8c1c
Abhyank <abhyankvarshney2006@gmail.com>
2021-11-22 14:49:17 +0530

    commit the initial java code

---
+++ question8largest3.java
@@ -0,0 +1,33 @@
+package assignement2;
+
+import java.util.Scanner;
+
+public class question8largest3
+{
+    public static void main (String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("enter first value");
+        a= scn.nextInt();
+        System.out.println("enter first value");
+        b= scn.nextInt();
+        System.out.println("enter first value");
+        c= scn.nextInt();
+        if((a>b)&&(a>c))
+        {
+            System.out.println("a will be greater among three");
+        }
+        else
+        {
+            if((b>a)&&(b>c))
+            {
+                System.out.println("b will be greater among three");
+            }
+            else
+            {
+                System.out.println("c will be greater among three");
+            }
+        }
+    }
+}
